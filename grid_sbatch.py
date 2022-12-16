29335fbad2fc7be4357f897098a07268bb5ce360
PatrickFeeney <patrickfeeney1225@gmail.com>
2022-12-16 16:06:05 -0500

    Grid Search

---
+++ grid_sbatch.py
@@ -0,0 +1,29 @@
+from argparse import ArgumentParser
+import itertools
+import subprocess
+
+import toml
+
+from sbatch import config_to_sbatch_flags, config_to_launch_cmd
+
+
+def search_to_flags(search_toml):
+    search_dict = toml.load(search_toml)
+    flag_strs = []
+    print(search_dict)
+    for vals in itertools.product(*search_dict.values()):
+        flag_strs.append(" ".join([f"{key} {val}" for key, val in zip(search_dict.keys(), vals)]))
+    return flag_strs
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("config_path")
+    parser.add_argument("search_path")
+    args = parser.parse_args()
+    sbatch_flags = config_to_sbatch_flags(args.config_path)
+    launch_cmd = config_to_launch_cmd(args.config_path)
+    flag_strs = search_to_flags(args.search_path)
+    for flag_str in flag_strs:
+        cmd = f"sbatch {sbatch_flags} --wrap \"{launch_cmd} {flag_str}\""
+        subprocess.run(f"{cmd} {flag_str}")
---
+++ sbatch.py
@@ -0,0 +1,25 @@
+from argparse import ArgumentParser
+import subprocess
+
+import toml
+
+
+def config_to_sbatch_flags(config_toml):
+    config_dict = toml.load(config_toml)
+    sbatch_flags = " ".join([f"{key} {val}" for key, val in config_dict["sbatch"].items()])
+    return sbatch_flags
+
+
+def config_to_launch_cmd(config_toml):
+    config_dict = toml.load(config_toml)
+    return config_dict["launch_cmd"]
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("config_path")
+    args = parser.parse_args()
+    sbatch_flags = config_to_sbatch_flags(args.config_path)
+    launch_cmd = config_to_launch_cmd(args.config_path)
+    cmd = f"sbatch {sbatch_flags} --wrap \"{launch_cmd}\""
+    subprocess.run(cmd)
